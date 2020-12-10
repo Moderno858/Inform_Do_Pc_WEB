ff391863492e7307cfd39a61a29a281f172f2f45
Moderno <71466911+Moderno858@users.noreply.github.com>
2020-12-09 23:13:15 -0300

    Summay

---
+++ Fundo.js
@@ -0,0 +1,16 @@
+function carregar(){
+var msg = window.document.getElementById('msg')
+ var data = new Date()
+var hora = date.getHours()
+msg.innerHTML = `Agora são ${hora}`
+if (hora >= 0 && hora < 12){
+    //BOM DIA
+
+//BOA TARDE
+}else if (hora >= 12 && hora < 18){
+
+}else{
+
+}
+//boa noite
+}
